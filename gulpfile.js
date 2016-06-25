890f53a1064cfc71c39173a79e393ff24dcac6b2
nhuanhoangduc <nhuan.hoangduc@outlook.com>
2016-06-26 02:33:09 +0700

    add minify js to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,47 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 
+// lint
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
-nodemon = require('gulp-nodemon')
+// minify js
+var minify = require('gulp-minify');
+
+// concat files
+var concat = require('gulp-concat');
+
+// node mon
+nodemon = require('gulp-nodemon');
 
 // lint server js files
 gulp.task('jslint Server', function() {
-  return gulp.src('./Server/**/*.js')
+  return gulp
+    .src('./Server/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish, { beep: true }));
 });
 
+
 // lint client js files
 gulp.task('jslint Client', function() {
-  return gulp.src('./Client/modules/**/*.js')
+  gulp
+    .src('./Client/modules/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish, { beep: true }));
 });
+
+
+// minify client js files
+gulp.task('minify client js files', function() {
+  gulp
+    .src('./Client/modules/**/*.js')
+    .pipe(concat('app.js'))
+    .pipe(minify())
+    .pipe(gulp.dest('./Client/production'));
+});
+
+
 
 gulp.task('start dev', function() {
   return nodemon({
@@ -26,4 +50,14 @@ gulp.task('start dev', function() {
   });
 });
 
-gulp.task('dev', ['jslint Server', 'jslint Client', 'start dev']);
+gulp.task('dev', function(done) {
+  console.log('Start gulp development');
+
+  runSequence(
+    'jslint Server',
+    'jslint Client',
+    'minify client js files',
+    done
+  );
+
+});
