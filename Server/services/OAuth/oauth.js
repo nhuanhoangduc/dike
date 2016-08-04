7e2b72e0f936c1a4f32ad4e3e0a5b1595d7ebbe0
nhuan <nhuan.hoangduc@outlook.com>
2016-08-04 11:29:55 +0700

    fix

--- Server/services/OAuth/oauth.js
+++ Server/services/OAuth/oauth.js
@@ -5,12 +5,12 @@ module.exports = {
     id: '1914286842131314',
     secret: '15de6083544e73e2e3d7f2adf7bf5141',
     token: '1914286842131314|UHVM5Ba95gHnaCQ4150aIhpwpcQ',
-    callbackURL: 'http://www.fudiemhen.com/users/login/facebook/callback',
 
     // id: '1927238587502806',
     // secret: '9257fc3bae5048cc164f20914e328494',
     // token: '1927238587502806|CR-0pLbwKr6HjRrTZvIRDFzsyNo',
-    // callbackURL: 'http://' + host.domain + ':' + host.port + '/users/login/facebook/callback',
+
+    callbackURL: 'http://' + host.domain + ':' + host.port + '/users/login/facebook/callback',
 
   }
 };
