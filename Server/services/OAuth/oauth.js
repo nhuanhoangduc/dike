e417c600902ff56d94d9e3cd4e50a6cd6c153455
nhuanhoangduc <nhuan.hoangduc@outlook.com>
2016-08-13 00:34:33 +0700

    ok

--- Server/services/OAuth/oauth.js
+++ Server/services/OAuth/oauth.js
@@ -4,13 +4,14 @@ module.exports = {
   facebook: {
     id: '1914286842131314',
     secret: '15de6083544e73e2e3d7f2adf7bf5141',
-    token: '1914286842131314|UHVM5Ba95gHnaCQ4150aIhpwpcQ',
+    token: '615fe1beaa113e967a01a03bb053fd5b',
+    callbackURL: 'http://www.fudiemhen.com/users/login/facebook/callback',
+
 
     // id: '1927238587502806',
     // secret: '9257fc3bae5048cc164f20914e328494',
     // token: '1927238587502806|CR-0pLbwKr6HjRrTZvIRDFzsyNo',
-
-    callbackURL: 'http://' + host.domain + ':' + host.port + '/users/login/facebook/callback',
+    //callbackURL: 'http://' + host.domain + ':' + host.port + '/users/login/facebook/callback',
 
   }
 };
