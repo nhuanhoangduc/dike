62c67ce9bda1201179c3b7a8c5782af2489b32d9
nhuanhoangduc <nhuan.hoangduc@outlook.com>
2016-08-17 00:59:33 +0700

    ok

--- Server/routes/admin.js
+++ Server/routes/admin.js
@@ -6,27 +6,27 @@ var adminServices = require('../services/adminServices');
 
 /* event */
 
-router.post('/events/:type', userServices.checkAdmin, adminServices.getEvents);
+router.post('/events/:type', adminServices.getEvents);
 
-router.get('/events/unblock/:type/:id', userServices.checkAdmin, adminServices.unBlockEvent);
-router.get('/events/block/:type/:id', userServices.checkAdmin, adminServices.blockEvent);
+router.get('/events/unblock/:type/:id', adminServices.unBlockEvent);
+router.get('/events/block/:type/:id', adminServices.blockEvent);
 
 /* / event */
 
 
 /* user */
 
-router.post('/users', userServices.checkAdmin, adminServices.getUsers);
+router.post('/users', adminServices.getUsers);
 
-router.get('/users/block/:id', userServices.checkAdmin, adminServices.blockUser);
-router.get('/users/unblock/:id', userServices.checkAdmin, adminServices.unBlockUser);
+router.get('/users/block/:id', adminServices.blockUser);
+router.get('/users/unblock/:id', adminServices.unBlockUser);
 
-router.get('/users/loadadmin', userServices.checkAdmin, adminServices.loadAdmin);
-router.get('/users/assignadmin/:facebookId', userServices.checkAdmin, adminServices.assignAdmin);
-router.get('/users/unassignadmin/:facebookId', userServices.checkAdmin, adminServices.unAssignAdmin);
+router.get('/users/loadadmin', adminServices.loadAdmin);
+router.get('/users/assignadmin/:facebookId', adminServices.assignAdmin);
+router.get('/users/unassignadmin/:facebookId', adminServices.unAssignAdmin);
 
-router.get('/users/autocomplete', userServices.checkAdmin, adminServices.userAutoComplete);
-router.get('/users/autocomplete/:name', userServices.checkAdmin, adminServices.userAutoComplete);
+router.get('/users/autocomplete', adminServices.userAutoComplete);
+router.get('/users/autocomplete/:name', adminServices.userAutoComplete);
 
 /*user */
 
